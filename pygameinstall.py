8e523fd6349b050e30b306799cb5fef2191e37f9
alexs551 <savagealexander411@gmail.com>
2026-02-14 13:47:59 +0000

    Fix path variable initialization in pygameinstall.py

--- pygameinstall.py
+++ pygameinstall.py
@@ -3,7 +3,7 @@
 import subprocess
 import sys
 
-path ="C:/Users/savag/OneDrive/Coding Projects/become_a_rectangle" # IMPORTANT: replace "" with the path to your folder and use / instead of \
+path ="" # IMPORTANT: replace "" with the path to your folder and use / instead of \
 
 def install(path):
     print("Installing pygame. This may take a while.")
@@ -12,5 +12,6 @@ def install(path):
         print("Successful install. You can now run \"main.py\"")
     except:
         print("There was an error installing. Please try running the script again.")
+
 
 install(path)
